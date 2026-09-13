0b12a655bbe274dea4e1483980bec7ee7a1e93c7
Preeti <preeti@Preetis-MacBook-Air.local>
2026-09-13 15:40:21 +0530

    test size changed to 0.4 in 7th algorithm

--- pythongrpproject/SGDC 60,40 (2).py
+++ pythongrpproject/SGDC 60,40 (2).py
@@ -35,7 +35,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
-    test_size=0.2,
+    test_size=0.4,
     random_state=0,
     stratify=y
 )
