03016ffa4d84b24e7aa3d25a8802a350863801b7
Preeti <preeti@Preetis-MacBook-Air.local>
2026-09-13 15:16:49 +0530

    7th algorithm with test size 0.2

--- pythongrpproject/SGDC 60,40 (2).py
+++ pythongrpproject/SGDC 60,40 (2).py
@@ -35,12 +35,12 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
-    test_size=0.6,
+    test_size=0.2,
     random_state=0,
     stratify=y
 )
-from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=200, criterion='entropy', random_state=0)
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
