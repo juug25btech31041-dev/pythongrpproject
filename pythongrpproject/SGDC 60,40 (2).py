8f720390b6558be3b46da4b653d8d5b9ede59835
Preeti <preeti@Preetis-MacBook-Air.local>
2026-09-16 11:21:09 +0530

    10th algorithm with test size 0.2

--- pythongrpproject/SGDC 60,40 (2).py
+++ pythongrpproject/SGDC 60,40 (2).py
@@ -35,12 +35,12 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
-    test_size=0.6,
+    test_size=0.2,
     random_state=0,
     stratify=y
 )
-from xgboost import XGBClassifier
-classifier = XGBClassifier()
+from lightgbm import LGBMClassifier
+classifier = LGBMClassifier()
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
