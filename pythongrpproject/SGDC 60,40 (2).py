49c0e719306fdde169ef1216ce4fc7a17535b812
Preeti <preeti@Preetis-MacBook-Air.local>
2026-09-16 11:10:16 +0530

    9th algorithm with test size 0.2

--- pythongrpproject/SGDC 60,40 (2).py
+++ pythongrpproject/SGDC 60,40 (2).py
@@ -35,12 +35,12 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
-    test_size=0.6,
+    test_size=0.2,
     random_state=0,
     stratify=y
 )
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators=200, criterion='entropy', random_state=0)
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
